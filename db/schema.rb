d266fd94b418ed8ee2820b1ed29fd60e0107f55e
taisuketoyonaga <69187127+taisuketoyonaga@users.noreply.github.com>
2020-09-16 16:17:04 +0900

    ActiveHashでフォームの実装

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2020_09_16_031924) do
 
   create_table "contents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "kind", null: false
+    t.integer "kind_id", null: false
     t.text "place"
     t.text "memo"
     t.date "date"
@@ -54,8 +54,6 @@ ActiveRecord::Schema.define(version: 2020_09_16_031924) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
