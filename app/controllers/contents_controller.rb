7399cfb98b0664f192bda867da34560d496895a4
taisuketoyonaga <69187127+taisuketoyonaga@users.noreply.github.com>
2020-09-18 20:00:44 +0900

    一覧表示の実装

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,7 +1,32 @@
 class ContentsController < ApplicationController
 
+
   def index
+  
+     @content = Content.all
+    
   end
 
+  def new
+    @content = Content.new
+  end
+
+  def create
+    @content =  Content.create(content_parameter)
+    if @content.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+private
   
+  def content_parameter
+    params.require(:content).permit(:image, :name, :kind_id, :place, :date, :memo).merge(user_id: current_user.id)
+  end
+
+  def set_params
+    @content = Content.find(params.id)
+  end
 end
--- app/models/content.rb
+++ app/models/content.rb
@@ -1,4 +1,10 @@
 class Content < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :kind
+   
+  validates :name, presence: true
+  validates :kind_id, presence: true
+
 end
