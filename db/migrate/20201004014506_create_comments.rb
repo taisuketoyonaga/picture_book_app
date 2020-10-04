46d8babdc6fde94ac88c57d689a811cbc309718d
taisuketoyonaga <69187127+taisuketoyonaga@users.noreply.github.com>
2020-10-04 10:50:20 +0900

    commentsテーブルの作成

--- db/migrate/20201004014506_create_comments.rb
+++ db/migrate/20201004014506_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-
+      t.text :comment , presence: true
+      t.references :user , presence: true
+      t.references :content, presence: true
       t.timestamps
     end
   end
