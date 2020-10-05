1e2992806ec7fb7380a0a99e703fa120d581d4b2
taisuketoyonaga <69187127+taisuketoyonaga@users.noreply.github.com>
2020-10-05 18:24:40 +0900

    コメント機能サーバーサイド実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,19 +3,17 @@ class CommentsController < ApplicationController
   def index
     @content = Content.find(params[:content_id])
     @comments = @content.comments.includes(:user)
-   # binding.pry
   end
   def create
     comment = Comment.create(comment_params)
-    if content.save
-      redirect_to content_comment_path(content_id:id)
-    end
+    render jason:{post: comment }
   end
 
-private
+ private
 
   def comment_params
     params.permit(:comment, :content_id).merge(user_id: current_user.id)
   end
   
 end
+
