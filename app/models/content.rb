739b7838b6c7d9cd29f7ab898725acf992f98841
taisuketoyonaga <69187127+taisuketoyonaga@users.noreply.github.com>
2020-10-06 15:58:53 +0900

    記事を削除するとコメントのデータも消える

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,6 +1,6 @@
 class Content < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments,dependent: :destroy
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :kind
